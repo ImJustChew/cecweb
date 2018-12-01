d0a96ef104c2ce35b957ae5c75f27e19c02bc95b
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2018-12-01 16:56:23 +0800

    Trying new effect
    Index archievements number transition

--- script/index.js
+++ script/index.js
@@ -206,3 +206,15 @@ function automatedUpdatesSlide() {
         nextUpdatesSlide();
     },5000);
 }
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
