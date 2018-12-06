2faa4a3bf5fac47dc8dafc9a6aa2dd13ab499088
Tenent Jason <15648200+tenent007963@users.noreply.github.com>
2018-12-06 19:39:02 +0800

    Update about.js
    Fuck it :)

--- script/about.js
+++ script/about.js
@@ -1,16 +1,20 @@
 function changelang(lang) {
 		switch (lang) {
 			case 1: 
-				$('en').fadeOut('slow', function(){ $('zh').fadeIn('slow');   });
+				$('#en').fadeOut('slow');
+				$('#zh').fadeIn('slow');
 				break;
 			case 2: 
-				$('zh').fadeOut('slow', function(){ $('jp').fadeIn('slow');   });
+				$('#zh').fadeOut('slow');
+				$('#jp').fadeIn('slow');
 				break;
 			case 3: 
-				$('jp').fadeOut('slow', function(){ $('de').fadeIn('slow');   });
+				$('#jp').fadeOut('slow');
+				$('#de').fadeIn('slow');
 				break;
 			case 4: 
-				$('de').fadeOut('slow', function(){ $('en').fadeIn('slow');   });
+				$('#de').fadeOut('slow');
+				$('#en').fadeIn('slow');
 				break;
 		}
 };
