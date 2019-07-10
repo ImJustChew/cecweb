b84c806c9b291b2dc59a115cadd2b24bb726bec2
ImJustChew <chewtzihwee@gmail.com>
2019-07-10 18:49:52 +0800

    added important links

---
+++ links/index.js
@@ -0,0 +1,9 @@
+$('document').ready(function(){
+  $('#borrow').click(function(){
+    window.location.assign("https://docs.google.com/forms/d/e/1FAIpQLSdu821LOCl4So2dnKIPphtkmksAfydxgHM_CoUsM0bTZkos-Q/viewform")
+  })
+  $('#name_list').click(function(){
+    window.location.assign('https://drive.google.com/file/d/1tMerUggqk0nz3caMT8yJrPQPzuZkQLk7/view')
+  })
+
+})
